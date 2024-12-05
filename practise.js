4fa36692c7983e2588ad0e4767bad0579d509523
Cameron Burchnall <cameron.burchnall@ntlworld.com>
2024-12-05 14:35:22 +0000

    First update

---
+++ practise.js
@@ -0,0 +1,10 @@
+let Username = prompt("Please Enter Username");
+
+console.log(`Hello ${Username}, nice to meet you`);
+
+console.log("Goodbye World!");
+
+let isCold = true;
+let isSunny = false;
+
+console.log(typeof Username);
